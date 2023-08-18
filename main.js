7d525d8ee9f9a6bcc8056c588955aa9559b6f9b5
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-08-18 13:00:09 -0700

    Scrape first page of personal leaderboard

--- main.js
+++ main.js
@@ -19,3 +19,44 @@ Extend:
 
 //https://jstris.jezevec10.com/cheese?display=5&user=justdumpedafatone&lines=100L
 
+const { JSDOM } = require( "jsdom" ); 
+
+const { window } = new JSDOM("", { 
+	url: "https://jstris.jezevec10.com/u/justdumpedafatone", 
+}); 
+const $ = require( "jquery" )( window ); 
+ 
+const runs = [];
+
+$.get("https://jstris.jezevec10.com/cheese?display=5&user=justdumpedafatone", function(html) { 
+    // get list of all 100L cheese runs
+	const cheeseRunsList = $(html).find("tr").filter(function() {
+        return this.tagName.toLowerCase() === "tr"; // Filter only <tr> elements
+    }); 
+
+	cheeseRunsList.each(function( i ) { 
+		const row = $(cheeseRunsList[i]);
+		// 8 attributes: #	Name	Time	Blocks	PPS	Finesse	Date	Replay
+		const runAttributes = row.find("td");
+		
+		if (typeof runAttributes[0] === "undefined") {
+			
+		} else {
+			const run = { 
+				number: $(runAttributes[0]).text(),
+				name: $(runAttributes[1]).text(),
+				time: $(runAttributes[2]).text(), 
+				blocks: $(runAttributes[3]).text(),
+				pps: $(runAttributes[4]).text(),
+				finesse: $(runAttributes[5]).text(),
+				date: $(runAttributes[6]).text(),
+				replay: $(runAttributes[7]).attr("href")
+			}; 
+
+			runs.push(run); 
+		}
+
+	}); 
+
+	console.log(JSON.stringify(runs)); 
+});

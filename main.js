92b6ed4346df74df3be2a312411b76dfbbdfcade
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-08-18 12:50:21 -0700

    Add outline

---
+++ main.js
@@ -0,0 +1,21 @@
+/* Outline:
+User's POV/Frontend:
+Website with a blank and submit button
+Input username
+->
+Backend:
+Search for the cheese page of that player - if not valid username, stop
+Collect every entry of cheese - move between pages by taking the next page link. while loop?
+Sort by lowest block count
+Display x many
+->
+Frontend:
+Displayed page of lowest cheese count with all original things. Perhaps a different aesthetic though.
+
+Extend:
+- other cheese modes
+- can choose max time/number displayed
+*/
+
+//https://jstris.jezevec10.com/cheese?display=5&user=justdumpedafatone&lines=100L
+
